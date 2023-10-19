1ba1214120eaf26939cf48f241f8d1e35dcf7800
Burhan Shaheen <hafiz.burhan834@gmail.com>
2023-10-19 14:34:54 +0500

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'new-version-angular-from-14';
+}
